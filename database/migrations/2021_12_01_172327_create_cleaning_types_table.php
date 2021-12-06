ba1ab716cb9a4843a5317b0e5f1385b991b9fb6b
michael <trillzglobal@gmail.com>
2021-12-06 23:09:48 +0100

    cleaning_types_table

--- database/migrations/2021_12_01_172327_create_cleaning_types_table.php
+++ database/migrations/2021_12_01_172327_create_cleaning_types_table.php
@@ -16,6 +16,10 @@ class CreateCleaningTypesTable extends Migration
     {
         Schema::create('cleaning_types', function (Blueprint $table) {
             $table->id();
+            $table->string('cleaning_type');
+            $table->float('unit_amount')->default(0.00);
+            $table->float('vat')->default(0.00);
+            $table->float('extra_charge')->default(0.00);
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
